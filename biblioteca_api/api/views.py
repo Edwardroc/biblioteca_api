313ef77c1831dfaab3cb03f09dc6a5c9dfd58314
Edwardroc <mdlzr13.gmail.com>
2025-09-07 11:58:10 -0500

    Creación de las vistas de la api

--- biblioteca_api/api/views.py
+++ biblioteca_api/api/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Autor, Editorial, Libro, Miembro, Prestamo
+from .serializers import AutorSerializer, EditorialSerializer, LibroSerializer, MiembroSerializer, PrestamoSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
-# Create your views here.
+class AutorListCreate(generics.ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+class AutorRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+class EditorialListCreate(generics.ListCreateAPIView):
+    queryset = Editorial.objects.all()
+    serializer_class = EditorialSerializer
+
+class EditorialRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Editorial.objects.all()
+    serializer_class = EditorialSerializer
+
+class LibroListCreate(generics.ListCreateAPIView):
+    queryset = Libro.objects.all()
+    serializer_class = LibroSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['autor', 'editorial']
+    search_fields = ['titulo', 'resumen', 'isbn']
+
+class LibroRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Libro.objects.all()
+    serializer_class = LibroSerializer
+
+class MiembroListCreate(generics.ListCreateAPIView):
+    queryset = Miembro.objects.all()
+    serializer_class = MiembroSerializer
+
+class MiembroRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Miembro.objects.all()
+    serializer_class = MiembroSerializer
+
+class PrestamoListCreate(generics.ListCreateAPIView):
+    queryset = Prestamo.objects.all()
+    serializer_class = PrestamoSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['fecha_prestamo', 'miembro']
+
+class PrestamoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Prestamo.objects.all()
+    serializer_class = PrestamoSerializer
